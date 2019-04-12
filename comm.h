6aee77f5b14d7f311ff5450297334988c25c3142
mohiuddinemon <emonmohiuddin@yahoo.com>
2019-04-12 08:39:01 +0000

    updating

---
+++ comm.h
@@ -0,0 +1,7 @@
+// constatnt definitions
+
+#define URL "http://www.cc.puv.fi/~e1700687/sound.php"
+
+// functions declarations
+
+void sendDATA(short d[]);
--- main.c
+++ main.c
@@ -7,11 +7,22 @@
 #include "sound.h"
 #include "comm.h"
 
-int main()
+int main(int argc, char **argv)
 {
 //	int i, arr[80];
 //	for(i=0; i<80; i++)
 //		arr[i] = rand()%70 + 30;
+	if(argc == 2)
+	{
+		int ch;
+		printf("how many channels? (1:mono, 2:stereo):");
+		scanf("%d", &ch);
+		float duration;
+		printf("how long is the test tone? (1-10 sec):");
+		scanf("%f", &duration);
+		testTone(ch, atoi(argv[1]), duration);
+		return 0;
+	}
 	FILE *f;
 	short sd[RATE];		// for all samples in 1 second
 	while(1)
